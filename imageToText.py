a6ee5a9ca8336e40c266d7be619bdf4d4a7421da
NoicTheBrave <nicpizza2000@gmail.com>
2024-03-24 18:43:18 -0500

    About to mesh together the image to text pgm with the rest of it (Still NOT saving data yet, just printing it to the consol for rn)

--- imageToText.py
+++ imageToText.py
@@ -5,19 +5,21 @@ from pytesseract import pytesseract
 
 # Defining paths to tesseract.exe 
 # and the image we would be using 
-path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-image_path = "C:\\Users\\nicpi\\OneDrive\\Documents\\Python_VideoToText_DroidScriptVideo_GPS\\GetGPSFromDroidScript\\demoImage.jpg"#r"csv\sample_text.png"
+def imageToText(image_path, fileName ): 
+    path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe" #program path that is used for Windows for the image-to-text pgm
+    
+    # Opening the image & storing it in an image object 
+    img = Image.open(image_path + "\\" + fileName) 
 
-# Opening the image & storing it in an image object 
-img = Image.open(image_path) 
+    # Providing the tesseract executable 
+    # location to pytesseract library 
+    pytesseract.tesseract_cmd = path_to_tesseract 
 
-# Providing the tesseract executable 
-# location to pytesseract library 
-pytesseract.tesseract_cmd = path_to_tesseract 
+    # Passing the image object to image_to_string() function 
+    # This function will extract the text from the image 
+    text = pytesseract.image_to_string(img) 
 
-# Passing the image object to image_to_string() function 
-# This function will extract the text from the image 
-text = pytesseract.image_to_string(img) 
+    # Displaying the extracted text 
+    print(text[:-1])
 
-# Displaying the extracted text 
-print(text[:-1])
+#imageToText("C:\\Users\\nicpi\\OneDrive\\Documents\\Python_VideoToText_DroidScriptVideo_GPS\\GetGPSFromDroidScript", "demoImage.jpg")
