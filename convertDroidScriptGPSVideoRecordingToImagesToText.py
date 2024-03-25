7e9ac979fdea78055393eb8ea066b6b3273248ab
NoicTheBrave <nicpizza2000@gmail.com>
2024-03-25 15:41:52 -0500

    updated gitignore so it SHOULD start forgetting image files going forward (I hope -_-) AND fixed an error where the image-to-text application confused a period with a comma, breaking the pgm. APPARENTLY THIS IS A COMMON ISSUE cause over ~200 pics, I've seen this issue be fixed 5-10 times, and there's a total of ~1800-2000 frames for my current demo video so THATS ALOT 0_0

--- convertDroidScriptGPSVideoRecordingToImagesToText.py
+++ convertDroidScriptGPSVideoRecordingToImagesToText.py
@@ -4,7 +4,7 @@
 import cv2 
 import re 
 
-DEVELOPMENT = 1 #0 = RUN AS NORMAL ||| 1 = DISABLES GENERATING NEW IMAGES, CROPPING THEM, AND ANY OTHER FEATURES DOWN THE ROAD AS NEED BE (mainly cause I dont need this done avery time to test 1 feature sooooo)
+DEVELOPMENT = 0 #0 = RUN AS NORMAL ||| 1 = DISABLES GENERATING NEW IMAGES, CROPPING THEM, AND ANY OTHER FEATURES DOWN THE ROAD AS NEED BE (mainly cause I dont need this done avery time to test 1 feature sooooo)
 
 # Function to extract frames 
 def FrameCapture(path, fileName): 
@@ -51,12 +51,19 @@ if __name__ == '__main__':
 	from imageToText import *
 	import os
 	# Calling the function 
-	#FrameCapture("Z:\\0_NewOBSOutput\\demo.mp4") #"C:\\Users\\Admin\\PycharmProjects\\project_1\\openCV.mp4") 
+	
+	#if DEVELOPMENT == 0: #if 0 -> Development is COMPLETE
+	#	FrameCapture("Z:\\0_NewOBSOutput\\demo.mp4") #"C:\\Users\\Admin\\PycharmProjects\\project_1\\openCV.mp4") 
+	
 	directory = "C:\\Users\\nicpi\\OneDrive\\Documents\\Python_VideoToText_DroidScriptVideo_GPS\\GetGPSFromDroidScript"
 	videoName = "demo.mp4"
  
 	#----------RE-ENABLE AFTER DEVELOPMENT IS COMPLETE------------
-	#counter = FrameCapture(directory,videoName)
+	if DEVELOPMENT == 0:
+		counter = FrameCapture(directory,videoName)
+	else:
+		counter = len(os.listdir(directory + "\\images\\"))
+		print("COUNTER: " + str(counter))
 
 
 
@@ -77,10 +84,12 @@ if __name__ == '__main__':
 		print("----------- " + str(i) + " -----------")
 		imageName = "frame" + str(i) + ".jpg"
 		crop(directory+ "\\images", imageName) #Crops the image file 
+
 		text = imageToText(directory + "\\images", imageName) #Extracts text from the image file
 		
 		#--------RE-ENABLE AFTER DEVELOPMENT IS COMPLETE: DELETE'S IMAGES AFTER DONE WITH PROCESS-----
-		#os.remove(directory + "\\images\\" + imageName) #deletes the image file once it has read the text
+		#if DEVELOPMENT == 0:
+		#	os.remove(directory + "\\images\\" + imageName) #deletes the image file once it has read the text
 
 		print("------------------------------")
 		print(text)
@@ -94,26 +103,27 @@ if __name__ == '__main__':
 		for i in text: 
 			temp = text[tempCounter].split(',') #Apparently sometimes PERIODS can be mistaken as commas... sooo
 			
-			#SOMETIMES the pgm mistakes "." -> "," Following Code-section corrects that
-			if len(temp) > 6: #should always be 6, unless it mistakens a period as a comma! then there's at min. 7 items in there 0_0
-				#If this happens, this means that ONE of these things CAN be directly converted from str -> int, so if it can, then combine current string cell with the cell to its left, then pop/remove the "full intiger" cell I just checked (i basically identify the fault, and re-append them as they should be) 
-				for i in range(len(temp)):
-					try: 
-						fixCommaErr = int(temp[i])
-						#next line of code should run if it works... allowing me to append previous thing with current, fixing the issue (and deleting the mistake from the array)
-						temp[i-1] = temp[i-1] + "." + temp[i]
-						print("ATTEMPT TO FIX ERROR: " + str(temp[i-1]))
-						time.sleep(5)
-					except:
-						print("Valid Cell, skipping...")
+			
 
 					
-			print(temp)
+			#print("TEMP: " + str(temp))
 			for j in temp:
 				holder.append(j)
 			#print()
 			tempCounter +=1
-		print(holder)
+		print("HOLDER" + str(holder))
+		#SOMETIMES the pgm mistakes "." -> "," Following Code-section corrects that
+		if len(holder) > 6: #should always be 6, unless it mistakens a period as a comma! then there's at min. 7 items in there 0_0
+			#If this happens, this means that ONE of these things CAN be directly converted from str -> int, so if it can, then combine current string cell with the cell to its left, then pop/remove the "full intiger" cell I just checked (i basically identify the fault, and re-append them as they should be) 
+			for i in range(len(holder)):
+				try: 
+					fixCommaErr = int(holder[i])
+					#next line of code should run if it works... allowing me to append previous thing with current, fixing the issue (and deleting the mistake from the array)
+					holder[i-1] = holder[i-1] + "." + holder[i]
+					print("ATTEMPT TO FIX ERROR: " + str(holder[i-1]))
+					time.sleep(5)
+				except:
+					print("Valid Cell, skipping...")
 
 		tempCounter = 0 
 		for i in holder: #make it ONLY numbers now 
